a532168b7e4021b373ffe536e084b7ae71c21374
IvanProgramming <vlasov.ivan.2004@gmail.com>
2021-06-07 18:47:29 +0300

    :books: Added Documentation for gadza_category.py

--- src/gadza_category.py
+++ src/gadza_category.py
@@ -1,8 +1,15 @@
 from gadza import Gadza
+from random import choice
 
 
 class GadzaCategory:
+    """ This is class for Category of Gadza """
+
     def __init__(self, key: str, *gadzas: Gadza):
+        """ Constructor of class
+        :param key: Key of category in gadzasData.json
+        :param gadzas: List of gadzas, listed in category
+        """
         self.gadzas = gadzas
         self.key = key
 
@@ -11,6 +18,7 @@ class GadzaCategory:
 
     @property
     def as_dict(self):
+        """ Returns category as it presented in gadzasData.json """
         category_dict = {}
         for gadza in self:
             category_dict[gadza.gadza_key] = gadza
